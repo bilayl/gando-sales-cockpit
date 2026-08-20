6e0c750efac1d872307cf30562881115e8512ab2
bilayl <127876971+bilayl@users.noreply.github.com>
2026-08-20 10:05:43 +0200

    feat: expose Room SD creation from Deal Room

--- app/(cockpit)/deal-room/page.tsx
+++ app/(cockpit)/deal-room/page.tsx
@@ -1,5 +1,11 @@
 import { SDDealRoomHub } from "@/components/sd-deal-room-hub";
+import { SDRoomCreateLauncher } from "@/components/sd-room-create-launcher";
 
 export default function Page() {
-  return <SDDealRoomHub />;
+  return (
+    <>
+      <SDRoomCreateLauncher />
+      <SDDealRoomHub />
+    </>
+  );
 }
