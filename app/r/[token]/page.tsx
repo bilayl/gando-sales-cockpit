ffe42a3c1f0fbf12810cd7a03d433381dc11dac3
bilayl <127876971+bilayl@users.noreply.github.com>
2026-08-20 13:03:00 +0200

    feat: place square banner below Deal Room navbar

--- app/r/[token]/page.tsx
+++ app/r/[token]/page.tsx
@@ -1,8 +1,8 @@
-import { PublicSDRoomV2 } from "@/components/public-sd-room-v2";
+import { PublicSDRoomV3 } from "@/components/public-sd-room-v3";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params;
-  return <PublicSDRoomV2 token={token} />;
+  return <PublicSDRoomV3 token={token} />;
 }
