ecdc71dad705ee39204fa7f5d59ee210976600e4
bilayl <127876971+bilayl@users.noreply.github.com>
2026-08-21 12:28:51 +0200

    Use public Deal Room V6

--- app/r/[token]/page.tsx
+++ app/r/[token]/page.tsx
@@ -1,9 +1,9 @@
-import { PublicSDRoomV5 } from "@/components/public-sd-room-v5";
+import { PublicSDRoomV6 } from "@/components/public-sd-room-v6";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function Page({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params;
-  return <PublicSDRoomV5 token={token} />;
+  return <PublicSDRoomV6 token={token} />;
 }
