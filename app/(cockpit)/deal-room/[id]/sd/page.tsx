2a9f7c23b739e69bc90dab6207d19e0bea9bbd56
bilayl <127876971+bilayl@users.noreply.github.com>
2026-08-31 20:56:04 +0200

    Remove Dealroom workspace from CRM route group

--- app/(cockpit)/deal-room/[id]/sd/page.tsx
+++
@@ -1,6 +0,0 @@
-import { SDRoomWorkspace } from "@/components/sd-room-workspace";
-
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <SDRoomWorkspace dealId={id} />;
-}
