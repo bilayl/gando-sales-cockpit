c02b05e1d152e5ea95f25c55d0fab258bec6d9b7
bilayl <127876971+bilayl@users.noreply.github.com>
2026-08-31 19:23:43 +0200

    Tighten KPI workspace spacing

--- components/kpi-workspace.tsx
+++ components/kpi-workspace.tsx
@@ -8,15 +8,15 @@ import type { KpiView } from "@/lib/kpi-views"
 
 export function KpiWorkspace({ view, canEdit }: { view: KpiView; canEdit: boolean }) {
   if (view === "overview") {
-    return <div className="min-w-0 p-4 lg:p-6"><KpiExecutiveOverview /></div>
+    return <div className="min-w-0 p-4 lg:p-5"><KpiExecutiveOverview /></div>
   }
 
   if (view === "funnel") {
-    return <div className="min-w-0 p-4 lg:p-6"><KpiFunnelShadcn canEdit={canEdit} /></div>
+    return <div className="min-w-0 p-4 lg:p-5"><KpiFunnelShadcn canEdit={canEdit} /></div>
   }
 
   if (view === "history") {
-    return <div className="min-w-0 p-4 lg:p-6"><KpiMonthlyShadcn canEdit={canEdit} /></div>
+    return <div className="min-w-0 p-4 lg:p-5"><KpiMonthlyShadcn canEdit={canEdit} /></div>
   }
 
   return <KpiDashboardOverview />
