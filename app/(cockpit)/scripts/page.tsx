20b52fa7c545138334602f33d4abac3dba414543
bilayl <127876971+bilayl@users.noreply.github.com>
2026-09-07 14:44:55 +0200

    Add scripts cockpit category

---
+++ app/(cockpit)/scripts/page.tsx
@@ -0,0 +1,7 @@
+import { SalesScriptsWorkspace } from "@/components/sales-scripts-workspace";
+
+export const dynamic = "force-dynamic";
+
+export default function ScriptsPage() {
+  return <SalesScriptsWorkspace />;
+}
